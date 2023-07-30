3dbec929f079d25e27817a922363267cc2b9ca5b
sakthidhasankarthiga <130735073+sakthidhasankarthiga@users.noreply.github.com>
2023-07-30 17:32:32 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+      id: 1,
+      name: "Sakthi",
+      username: "@Black_venom_scorp_93",
+    },
+    {
+      id: 2,
+      name: "Ilaiyaraja",
+      username: "Yuvanshankar_raja",
+    },
+  ];
+  
+  export default userList;
